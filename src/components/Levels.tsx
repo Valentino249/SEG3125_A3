1c50de50c94a676910593d80ba60946e7154d084
Valentino249 <valentino.vinod@gmail.com>
2026-06-21 15:49:07 -0400

    Level buttons

--- src/components/Levels.tsx
+++ src/components/Levels.tsx
@@ -11,7 +11,18 @@ function Levels({ onNavigate }: LevelsProps) {
                 <p className="text-center">Levels</p>
                 <button className="mt-2 mb-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                 onClick={() => onNavigate("home")}>Home</button>
-                <Board/>
+                <div className="text-center flex flex-col">
+                    <button className="mb-2
+                    bg-red-300 hover:bg-red-200 text-white font-bold py-2 px-4 rounded">Level 1</button>
+                    <button className="mb-2
+                    bg-red-400 hover:bg-red-200 text-white font-bold py-2 px-4 rounded">Level 2</button>
+                    <button className="mb-2
+                    bg-red-500 hover:bg-red-200 text-white font-bold py-2 px-4 rounded">Level 3</button>
+                    <button className="mb-2
+                    bg-red-600 hover:bg-red-200 text-white font-bold py-2 px-4 rounded">Level 4</button>
+                    <button className="mb-2
+                    bg-red-700 hover:bg-red-200 text-white font-bold py-2 px-4 rounded">Level 5</button>
+                </div>
             </div>
         </>
     )
